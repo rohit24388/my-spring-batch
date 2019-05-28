09de04debec28416c82ae732b4895e92fcbd200e
rohit24388 <ramangaya.iem@gmaail.com>
2019-05-27 21:33:32 -0500

    Added a Spring Batch runner with an embedded HSQL database. Uses Hibernate reader.

---
+++ src/main/java/learning/example/springbatch/BatchConfiguration.java
@@ -0,0 +1,70 @@
+package learning.example.springbatch;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.HibernateCursorItemReader;
+import org.springframework.batch.item.database.builder.HibernateCursorItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+	@Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+    
+    @Autowired
+    public DataSource dataSource;
+    
+    @Autowired
+    public EntityManagerFactory entityManagerFactory;
+    
+	@Bean
+	public HibernateCursorItemReader hibernateReader() {
+		SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+		return new HibernateCursorItemReaderBuilder<Person>().name("personReader")
+				.sessionFactory(sessionFactory).queryString("from Person").build();
+    }
+	
+	public ItemWriter<Person> writer() {
+		ItemWriter<Person> writer = (List<? extends Person> persons) -> {
+			for (Person person : persons) {
+				System.out.println("Processing " + person);
+			}
+		};
+		return writer;
+	}
+	
+	@Bean
+	public Job displayUserJob(Step step1) {
+		return jobBuilderFactory.get("displayPersonJob")
+//            .incrementer(new RunIdIncrementer())
+//            .listener(listener)
+				.flow(step1).end().build();
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1").<Person, Person>chunk(10).reader(hibernateReader())
+//            .processor(processor())
+				.writer(writer()).build();
+	}
+
+
+}
---
+++ src/main/java/learning/example/springbatch/Person.java
@@ -0,0 +1,51 @@
+package learning.example.springbatch;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	public Person() {
+		
+	}
+	
+	public Person(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "first_name: " + firstName + ", last_name: " + lastName;
+	}
+}
